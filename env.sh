08bb35b1ddef403ce58f192bef4cea3691966ba4
senthil <senthil@scancafe.net>
2017-10-04 01:07:36 +0530

    [Config] Config changes

--- env.sh
+++ env.sh
@@ -4,3 +4,21 @@ export BRANCHIO_API_ENDPOINT="https://api.branch.io/v1/url"
 export SHARE_PREVIEW_URL="http://api.local/share/order-image-urls/"
 export SEND_LINK_URL="https://apidev.photogurus.com/send-app-link/"
 export ASSEST_URL="https://imgd.photogurus.com"
+export GOOGLE_PLAY_ID="https://play.google.com/store/apps/details?id=com.photogurus"
+export APPLE_STORE_ID="https://itunes.apple.com/us/app/photogurus/id799452318?ls=1&mt=8"
+export SEND_LINK_TEXT="cewe link"
+export EMAIL_PLACE_HOLDER="ductch your email"
+export LAST_PAGE_TEXT1="ductch Story looks awesome in app."
+export LAST_PAGE_TEXT2="ductch Download on your device!'"
+export EMAIL_VALIDATION1="ductch Please enter the email address"
+export EMAIL_VALIDATION2="ductch Invalid email address"
+export PAGE_TITLE="Preview"
+export PARTNER_CODE="cewe"
+export PARTNER_NAME="CEWE"
+
+
+
+
+
+
+
